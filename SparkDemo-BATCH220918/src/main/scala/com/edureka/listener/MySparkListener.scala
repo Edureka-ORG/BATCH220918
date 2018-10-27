02791f514378eb97efa2fbcb61ce895bf02024c9
svaduka <sainagaraju.vaduka@gmail.com>
2018-10-27 23:41:28 +0530

    MySparkListener

---
+++ SparkDemo-BATCH220918/src/main/scala/com/edureka/listener/MySparkListener.scala
@@ -0,0 +1,86 @@
+package com.edureka.listener
+
+import org.apache.spark.scheduler.SparkListener
+import org.apache.spark.scheduler.SparkListenerTaskGettingResult
+import org.apache.spark.scheduler.SparkListenerBlockManagerRemoved
+import org.apache.spark.scheduler.SparkListenerJobEnd
+import org.apache.spark.scheduler.SparkListenerEnvironmentUpdate
+import org.apache.spark.scheduler.SparkListenerEvent
+import org.apache.spark.scheduler.SparkListenerUnpersistRDD
+import org.apache.spark.scheduler.SparkListenerTaskEnd
+import org.apache.spark.scheduler.SparkListenerApplicationEnd
+import org.apache.spark.scheduler.SparkListenerApplicationStart
+import org.apache.spark.scheduler.SparkListenerExecutorMetricsUpdate
+import org.apache.spark.scheduler.SparkListenerTaskStart
+import org.apache.spark.scheduler.SparkListenerExecutorRemoved
+import org.apache.spark.scheduler.SparkListenerJobStart
+import org.apache.spark.scheduler.SparkListenerStageCompleted
+import org.apache.spark.scheduler.SparkListenerBlockUpdated
+import org.apache.spark.scheduler.SparkListenerStageSubmitted
+import org.apache.spark.scheduler.SparkListenerExecutorAdded
+import org.apache.spark.scheduler.SparkListenerBlockManagerAdded
+import org.apache.spark.util.JsonProtocolWrapper
+import org.apache.spark.SparkContext
+import org.apache.kafka.clients.producer.KafkaProducer
+import com.edureka.producer.ProducerUtil
+
+class MySparkListener extends SparkListener {
+
+  var producer: KafkaProducer[String, String] = null;
+  
+  val topic = "FLUME-BATCH220918-TOPIC";
+  
+  private def publishEventInternal(event: SparkListenerEvent) 
+  {
+    var appId = SparkContext.getOrCreate().applicationId
+    val eventJson = JsonProtocolWrapper.sparkEventToJson(event);
+    
+    producer = ProducerUtil.getProducer();
+    
+    ProducerUtil.send(producer, topic, appId,eventJson.toString());
+    
+
+  }
+
+  override def onStageCompleted(event: SparkListenerStageCompleted): Unit = {
+    publishEventInternal(event);
+  }
+
+  override def onStageSubmitted(event: SparkListenerStageSubmitted): Unit = {
+    publishEventInternal(event);
+  }
+
+  override def onTaskStart(event: SparkListenerTaskStart): Unit = {
+    publishEventInternal(event);
+  }
+
+  override def onTaskEnd(event: SparkListenerTaskEnd): Unit = {
+    publishEventInternal(event);
+  }
+
+  override def onJobStart(event: SparkListenerJobStart): Unit = {
+    publishEventInternal(event);
+  }
+
+  override def onJobEnd(event: SparkListenerJobEnd): Unit = {
+    publishEventInternal(event);
+  }
+
+  override def onApplicationStart(event: SparkListenerApplicationStart): Unit = {
+    publishEventInternal(event);
+  }
+
+  override def onApplicationEnd(event: SparkListenerApplicationEnd): Unit = {
+    publishEventInternal(event);
+    ProducerUtil.close(producer);
+  }
+
+  override def onExecutorAdded(event: SparkListenerExecutorAdded): Unit = {
+    publishEventInternal(event);
+  }
+
+  override def onExecutorRemoved(event: SparkListenerExecutorRemoved): Unit = {
+    publishEventInternal(event);
+  }
+
+}
